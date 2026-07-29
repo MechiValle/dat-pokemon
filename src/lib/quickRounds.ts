e4763c68f553bd44ab4f1bdcf178adceea5b1c57
Mercedes <Mercedesvalle17@gmail.com>
2026-07-29 04:47:36 -0300

    gameplay adjustment

--- src/lib/quickRounds.ts
+++ src/lib/quickRounds.ts
@@ -2,7 +2,7 @@ import { Pokemon } from "@/types/pokemon";
 import { shuffle } from "./shuffle";
 
 export const QUICK_ROUND_COUNT = 30;
-export const QUICK_ROUND_SECONDS = 180;
+export const QUICK_ROUND_SECONDS = 240;
 
 export function selectQuickPool(fullPool: Pokemon[]): Pokemon[] {
   return shuffle(fullPool).slice(0, QUICK_ROUND_COUNT);
